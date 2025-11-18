a0246653f9da30333d95226d9bc3b9cac25ee3ea
Elmojtaba <Elmojtaba.mohamed@studnet.snow.edu>
2025-11-19 03:56:15 +0400

    finsihed lab 3

--- Program.cs
+++ Program.cs
@@ -1,63 +1,142 @@
-﻿class Program
-{
-    static void Main()
-    {
-        string[] pixelRows = new string[] {
-"..............",
-"..............",
-"....RRRRR.....",
-".KKKKKKKKKKKK.",
-"..KSKSSSKSSS..",
-"..KSKKSSSKSSS.",
-"..KKSSSSKKKK..",
-"....SSSSSSS...",
-"...BBRBBB.....",
-"..BBBRBBRBBB..",
-".BBBBRBBRBBBB.",
-".SSBBRRRRBBSS.",
-".SSSRYRRYRSSS.",
-".SSRRRRRRRRSS.",
-"...RRR..RRR...",
-"..OOO...OOO...",
-".OOOO...OOOO..",
-".............."
-        };
+﻿Console.Clear();
 
-        Console.Clear();
-        Console.BackgroundColor = ConsoleColor.Cyan;
-        Console.Clear();
+Console.SetCursorPosition(3, 1);
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("                  ");
 
-        for (int row = 0; row < pixelRows.Length; row++)
-        {
-            string line = pixelRows[row];
-            Console.SetCursorPosition(0, row);
+Console.SetCursorPosition(2, 2);
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("                        ");
 
-            int c = 0;
-            while (c < line.Length)
-            {
-                char ch = line[c];
-                int run = 1;
-                while (c + run < line.Length && line[c + run] == ch) run++;
 
-                Console.BackgroundColor = ColorFor(ch);
-                Console.Write(new string(' ', run * 3));
+Console.SetCursorPosition(2, 3);
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("      ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("    ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("      ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("  ");
 
-                c += run;
-            }
-        }
+Console.SetCursorPosition(1,4);
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("    ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("      ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("      ");
 
-        Console.ReadKey();
-    }
+Console.SetCursorPosition(1, 5);
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("    ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("        ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("    ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("  ");
 
-    static ConsoleColor ColorFor(char ch)
-    {
-        if (ch == 'R') return ConsoleColor.Red;
-        if (ch == 'B') return ConsoleColor.Blue;
-        if (ch == 'S') return ConsoleColor.DarkYellow;
-        if (ch == 'K') return ConsoleColor.Black;
-        if (ch == 'Y') return ConsoleColor.Yellow;
-        if (ch == 'O') return ConsoleColor.DarkYellow;
-        return ConsoleColor.Cyan;
-    }
-}
+Console.SetCursorPosition(1, 6);
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("      ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("          ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("      ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("  ");
 
+
+Console.SetCursorPosition(2, 7);
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("                      ");
+
+Console.SetCursorPosition(3, 8);
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("  ");
+Console.SetCursorPosition(7,8);
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("    ");
+
+Console.SetCursorPosition(2, 9);
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("  ");
+Console.SetCursorPosition(7, 9);
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("    ");
+
+Console.SetCursorPosition(0, 10);
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("    ");
+Console.SetCursorPosition(8,10);
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("      ");
+Console.SetCursorPosition(18, 10);
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("    ");
+
+Console.SetCursorPosition(0, 11);
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("      ");
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("    ");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("      ");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("    ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("    ");
+
+Console.SetCursorPosition(1,12);
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("    ");
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("                ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("    ");
+
+Console.SetCursorPosition(2, 13);
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("        ");
+Console.SetCursorPosition(14,13);
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("        ");
+
+
+Console.SetCursorPosition(1, 14);
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.Write("        ");
+Console.SetCursorPosition(14, 14);
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.Write("        ");
+
+Console.SetCursorPosition(0, 15);
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.Write("          ");
+Console.SetCursorPosition(14, 15);
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.Write("        ");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.SetCursorPosition(0, 17);
+Console.WriteLine();
